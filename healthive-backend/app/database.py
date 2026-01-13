74c0b5322f69c9501b2701b6ea9ab68e43f93aa3
nah-ais <muhamadsandialfarizi@gmail.com>
2026-01-13 20:04:29 +0700

    Delete healthive-backend/app directory

--- healthive-backend/app/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "mysql+pymysql://root:Faizmsa311!@localhost/calorie_app"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-Base = declarative_base()
